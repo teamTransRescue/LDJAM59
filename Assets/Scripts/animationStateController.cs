de8235165c7039a7f006231c90afadf389e32340
Anne Ogborn <annie66us@yahoo.com>
2026-03-17 10:57:41 +0100

    animation working with Amy character but no motion

---
+++ Assets/Scripts/animationStateController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash = Animator.StringToHash("isWalking");
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool walking = Input.GetKey("w");
+        bool isWalking = animator.GetBool(isWalkingHash);
+
+        if (walking != isWalking)
+        {
+            animator.SetBool(isWalkingHash, walking);
+        }
+    }
+}
